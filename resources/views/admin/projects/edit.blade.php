1ed9b9d0451a667906866d232fbdcde56a7a41bf
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-29 17:36:58 +0100

    [ADD]: edit blade php

---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="edit">
+        <h1>Edit Project</h1>
+        <form action="{{ route('admin.projects.update', $project) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ $project->name }}">
+            </div>
+            <div class="mb-3">
+                <label for="date_start" class="form-label">Date start</label>
+                <input type="date" name="date_start" class="form-control" id="date_start" value="{{ $project->date_start }}">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">description</label>
+                <textarea class="form-control" id="description" rows="3" name="description">{{ $project->description }}</textarea>
+            </div>
+        @endsection
