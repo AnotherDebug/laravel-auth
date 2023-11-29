138c2f5fbafa815819f5df55de91fefabcb1d653
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-29 15:54:01 +0100

    [ADD]: table in admin index

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="projectList m-5">
+    <h1>Projects List</h1>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+@endsection
