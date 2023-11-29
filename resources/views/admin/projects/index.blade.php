083fa9cdd210d5cb6f9327b111dac862f2044ddc
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-29 16:39:40 +0100

    [ADD]: Project values in table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,29 +1,38 @@
 @extends('layouts.admin')
 
+
+
 @section('content')
 
-<div class="projectList m-5">
+<div class="projectList ms-5 pt-5">
     <h1>Projects List</h1>
 
 <table class="table">
     <thead>
       <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">Id</th>
+        <th scope="col">Name</th>
+        <th scope="col">Date_start</th>
+        <th scope="col">Description</th>
+        <th scope="col">Azioni</th>
       </tr>
     </thead>
     <tbody>
+      @foreach ($projects as $project)
       <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
+        <td>{{ $project->id }}</td>
+        <td>{{ $project->name }}</td>
+        <td>{{ date('d/m/Y', strtotime($project->date_start)) }}</td>
+        <td>{{ $project->description }}</td>
+        <td>X</td>
       </tr>
+      @endforeach
 
     </tbody>
   </table>
+  {{ $projects->links() }}
 </div>
+
+
 
 @endsection
