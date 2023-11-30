29db75fd554a1ca9f1fc96f3a1c5381a490bcafc
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-30 15:14:19 +0100

    [ADD]: data seeder

--- database/seeders/TechnologiesSeeder.php
+++ database/seeders/TechnologiesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Technology;
+use Illuminate\Support\Str;
 
 class TechnologiesSeeder extends Seeder
 {
@@ -21,7 +22,8 @@ class TechnologiesSeeder extends Seeder
 
             $new_technology = new Technology();
             $new_technology->name = $technology;
-
+            $new_technology->slug = Str::slug($technology, '-');
+            $new_technology->save();
         }
     }
 }
--- database/seeders/TypesSeeder.php
+++ database/seeders/TypesSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Type;
 
 class TypesSeeder extends Seeder
 {
@@ -14,6 +16,14 @@ class TypesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = ['CSS', 'HTML', 'Javascript', 'PHP', 'Laravel', 'Bootstrap', 'Wordpress', 'Jquery', 'React', 'Vue', 'Angular'];
+
+        foreach ($data as $type) {
+
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug($type, '-');
+            $new_type->save();
+        }
     }
 }
