33dfccaeaf054f926e64ceb09e085dd41c70d84b
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-30 16:01:43 +0100

    [ADD]: view technologies index

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Technology;
 
 class TechnologyController extends Controller
 {
@@ -14,7 +15,8 @@ class TechnologyController extends Controller
      */
     public function index()
     {
-        //
+        // $technologies = Technology::all();
+        return view('admin.technologies.index');
     }
 
     /**
